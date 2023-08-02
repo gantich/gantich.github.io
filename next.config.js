74fbe3a64f0618cc6ce4aa7d42a6a171253c4284
Guillermo Antich <gaantich@mediapro.tv>
2023-08-02 10:54:50 +0200

    Update build script and config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+  themeConfig: './theme.config.js',
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 })
 
