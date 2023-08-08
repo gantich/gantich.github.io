58b239fbed9e39e8efec84ab86b05d93146e5aeb
Guillermo Antich <gaantich@mediapro.tv>
2023-08-08 09:02:13 +0200

    Add svg and edit documentation

--- next.config.js
+++ next.config.js
@@ -10,6 +10,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack(config) {
+    const allowedSvgRegex = /components\/icons\/.+\.svg$/
+
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test('.svg')
+    )
+    fileLoaderRule.exclude = allowedSvgRegex
+
+    config.module.rules.push({
+      test: allowedSvgRegex,
+      use: ['@svgr/webpack']
+    })
+    return config
+  }
 }
 
 module.exports = withNextra(nextConfig)
